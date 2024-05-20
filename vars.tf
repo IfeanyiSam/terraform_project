f17f42791ad785c30ad5ce2dc125f9ab770d5afa
Samuel Nnanna <samuelnnanna71@gmail.com>
2024-05-20 01:51:59 +0100

    test terraform scripts

---
+++ vars.tf
@@ -0,0 +1,90 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0e001c9271cf7f3b9"
+  }
+}
+
+variable "PRIV_KEY" {
+  default = "testkey"
+}
+
+variable "PUB_KEY" {
+  default = "testkey.pub"
+}
+
+variable "USERNAME" {
+  default = "ubuntu"
+}
+
+variable "RMQUSER" {
+  default = "rabbit"
+}
+
+variable "RMQPASS" {
+  default = "syco@123456"
+}
+
+variable "DBNAME" {
+  default = "accounts"
+}
+
+variable "DBPASS" {
+  default = "admin123"
+}
+
+variable "DBUSER" {
+  default = "admin"
+}
+
+variable "ins_cnt" {
+  default = "1"
+}
+
+variable "VPC_NAME" {
+  default = "terraform_vpc"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "ZONE3" {
+  default = "us-east-1c"
+}
+
+variable "VPC_CIDR" {
+  default = "172.21.0.0/16"
+}
+
+variable "PUBSUB1CIDR" {
+  default = "172.21.1.0/24"
+}
+
+variable "PUBSUB2CIDR" {
+  default = "172.21.2.0/24"
+}
+
+variable "PUBSUB3CIDR" {
+  default = "172.21.3.0/24"
+}
+
+variable "PRIVSUB1CIDR" {
+  default = "172.21.4.0/24"
+}
+
+variable "PRIVSUB2CIDR" {
+  default = "172.21.5.0/24"
+}
+
+variable "PRIVSUB3CIDR" {
+  default = "172.21.6.0/24"
+}
---
+++ vpc.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+    source = "terraform-aws-module/vpc/aws"
+
+    name = var.VPC_NAME
+    cidr = var.VPC_CIDR
+    azs = [var.ZONE1, var.ZONE2, var.ZONE3]
+    private_subnets = [var.PRIVSUB1CIDR, var.PRIVSUB2CIDR, var.PRIVSUB3CIDR]
+    public_subnets = [var.PUBSUB1CIDR, var.PUBSUB2CIDR, var.PUBSUB3CIDR]
+    enable_nat_gateway = true
+    single_nat_gateway = true
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags = {
+        Terraform = "true"
+        Environment = "test"
+    }
+    vpc_tags = {
+        Name = var.VPC_NAME
+    }
+}
